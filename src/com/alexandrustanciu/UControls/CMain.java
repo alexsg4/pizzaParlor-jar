aceaa3e42116c9bf393031da703feafb8d2d7de7
Alexandru Stanciu <stanciu.alex74@gmail.com>
2018-08-04 13:27:45 +0300

    Cleanup sidebar button event syntax

--- src/com/alexandrustanciu/UControls/CMain.java
+++ src/com/alexandrustanciu/UControls/CMain.java
@@ -4,16 +4,12 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 
 public class CMain{
 
     @FXML Pane mainPane;
-    @FXML private Button btnOverview;
-    @FXML private Button btnInventory;
-    @FXML private Button btnMenu;
-    @FXML private Button btnOrders;
+    @FXML private Button btnOverview, btnOrders, btnMenu, btnInventory;
     @FXML private ImageView logoView;
 
     @FXML
@@ -30,28 +26,20 @@ public class CMain{
     }
 
     private void buildSideBar(ScreenController controller) {
+
         logoView.setImage(new Image("/png/053-pizza.png"));
 
-        //TODO update screens after setting them
         btnOverview.setText(EScreens.OVER.toString());
-        btnOverview.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent ->{
-            controller.setScreen(EScreens.OVER.toString());
-        });
+        btnOverview.setOnAction( mouseEvent -> controller.setScreen(EScreens.OVER.toString()));
 
         btnMenu.setText(EScreens.MENU.toString());
-        btnMenu.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent ->{
-            controller.setScreen(EScreens.MENU.toString());
-        });
+        btnMenu.setOnAction( mouseEvent -> controller.setScreen(EScreens.MENU.toString()));
 
         btnOrders.setText(EScreens.ORDR.toString());
-        btnOrders.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent ->{
-            controller.setScreen(EScreens.ORDR.toString());
-        });
+        btnOrders.setOnAction( mouseEvent -> controller.setScreen(EScreens.ORDR.toString()));
 
         btnInventory.setText(EScreens.INVT.toString());
-        btnInventory.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent ->{
-            controller.setScreen(EScreens.INVT.toString());
-        });
+        btnInventory.setOnAction( mouseEvent -> controller.setScreen(EScreens.INVT.toString()));
     }
 
 }
