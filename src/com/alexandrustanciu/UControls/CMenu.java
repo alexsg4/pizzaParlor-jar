a16af82accf39f3ad138081285707b8030734fe0
Alexandru Stanciu <stanciu.alex74@gmail.com>
2018-08-04 13:55:00 +0300

    Add placeholders for Orders and Menu screens

---
+++ src/com/alexandrustanciu/UControls/CMenu.java
@@ -0,0 +1,28 @@
+package com.alexandrustanciu.UControls;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+
+public class CMenu extends ControlledScreen {
+
+    @FXML private FlowPane contentPane;
+    @FXML private BorderPane widget;
+
+    @Override
+    public void initialize() {
+
+        AnchorPane.setTopAnchor(contentPane, 0d);
+        AnchorPane.setRightAnchor(contentPane, 0d);
+        AnchorPane.setBottomAnchor(contentPane, 0d);
+        AnchorPane.setLeftAnchor(contentPane, 0d);
+
+        //TODO implement
+    }
+
+    private void populate(){
+        //TODO implement
+    }
+}
+
